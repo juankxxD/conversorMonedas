c9ea19bb5731596f738236b19029da86df88cbd4
juankxxD <juangamerospina@gmail.com>
2023-02-18 10:27:39 -0500

    Se agregaron constructores, getters y setters de la clase moneda

--- conversor/src/conversor/Moneda.java
+++ conversor/src/conversor/Moneda.java
@@ -1,5 +1,30 @@
 package conversor;
 
 public class Moneda {
-
+	
+	private String nombre;
+	private double cantidad;
+	
+	
+	
+	public Moneda(String nombre, double cantidad) {
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+	}
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	
 }
